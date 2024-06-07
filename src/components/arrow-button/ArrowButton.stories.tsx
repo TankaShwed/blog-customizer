1a382e6eefcc284b4233f74e28fb38b462f72207
Tanya Shvedyuk <shwedyuk.tatyana@yandex.ru>
2024-06-07 16:32:01 +0300

    arrow button

--- src/components/arrow-button/ArrowButton.stories.tsx
+++ src/components/arrow-button/ArrowButton.stories.tsx
@@ -13,7 +13,9 @@ export const ArrowButtonStory: Story = {
 	render: () => {
 		return (
 			<>
-				<ArrowButton />
+				<ArrowButton isOpen={false} toggleForm={function (): void {
+					throw new Error('Function not implemented.');
+				} } />
 			</>
 		);
 	},
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,24 @@
+import clsx from 'clsx';
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
+import { useState } from 'react';
 
 import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggleForm = () => {
+		setIsOpen(!isOpen);
+	};
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isOpen} toggleForm={toggleForm} />
+
+			<aside className={clsx(styles.container, {[styles.container_open] : isOpen})}>
 				<form className={styles.form}>
+
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
