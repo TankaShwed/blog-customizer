28042f25226f52134848236bc00c5d96a1e7e832
Tanya Shvedyuk <shwedyuk.tatyana@yandex.ru>
2024-06-08 14:40:42 +0300

    ащте

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,15 +2,26 @@ import clsx from 'clsx';
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 import { useState } from 'react';
+import { OptionType } from 'src/constants/articleProps';
+import { Select } from '../select';
 
 import styles from './ArticleParamsForm.module.scss';
 
+const fontOptions: OptionType[] = [
+	{title: 'Open Sans', value: 'openSans', className: ''},
+	{title: 'Ubuntu', value: 'ubuntu', className: ''},
+	{title: 'Cormorant Garamond', value: 'cormorantGaramond', className: ''},
+	{title: 'Days One', value: 'daysOne', className: ''},
+	{title: 'Merriweather', value: 'merriweather', className: ''}
+]
+
 export const ArticleParamsForm = () => {
 	const [isOpen, setIsOpen] = useState(false);
-
 	const toggleForm = () => {
 		setIsOpen(!isOpen);
 	};
+
+	const [font, setFont] = useState<OptionType>(fontOptions[0]);
 
 	return (
 		<>
@@ -18,6 +29,9 @@ export const ArticleParamsForm = () => {
 
 			<aside className={clsx(styles.container, {[styles.container_open] : isOpen})}>
 				<form className={styles.form}>
+
+					<h2 className='H2'>Задайте параметры</h2>
+					<Select selected={font} options={fontOptions} onChange={setFont} title="шрифт"/>
 
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
